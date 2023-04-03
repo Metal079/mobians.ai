b91c0cf4bf2b259645577acbb40d80a6f7ad4c2e
Pablo Castro <pablocastro079@gmail.com>
2023-04-03 01:06:56 -0500

    attempt at fixing favicon

---
+++ main_page/sitemaps.py
@@ -0,0 +1,12 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'daily'
+
+    def items(self):
+        return ['main_page:index']
+
+    def location(self, item):
+        return reverse(item)
